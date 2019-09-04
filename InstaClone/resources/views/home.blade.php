3345c19d03282af24c3a429c5a9a5a7dc403c412
David Moya <davidmgarcia6@gmail.com>
2019-09-04 21:31:26 +0100

    Design finish

--- InstaClone/resources/views/home.blade.php
+++ InstaClone/resources/views/home.blade.php
@@ -2,22 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+   <div class="row">
+       <div class="col-3 p-5">
+           <img src="https://instagram.fdub3-1.fna.fbcdn.net/vp/e8c5c6ddae53e75a134bd93e201d0e36/5E038B38/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fdub3-1.fna.fbcdn.net" class="rounded-circle">
+       </div>
+       <div class="col-9 pt-5">
+            <div><h1>clone of Insta</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+           <div class="pt-4 font-weight-bold">dmglion.com</div>
+           <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+           <div><a href="http://dmglion.com/#/">dmglion.com</a></div>
+       </div>
+   </div>
+   <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://www.rocksins.com/wp-content/uploads/2019/07/cropped-image100000-3.jpg" class="w-100">
         </div>
-    </div>
+       <div class="col-4">
+           <img src="https://www.rocksins.com/wp-content/uploads/2019/07/cropped-image100000-3.jpg" class="w-100">
+       </div>
+       <div class="col-4">
+           <img src="https://www.rocksins.com/wp-content/uploads/2019/07/cropped-image100000-3.jpg" class="w-100">
+       </div>
+   </div>
 </div>
 @endsection
