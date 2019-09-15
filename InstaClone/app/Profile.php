a47cbf147d9675b99f76af5aa01ce19ce2ca3e68
David Moya <davidmgarcia6@gmail.com>
2019-09-15 19:57:57 +0100

    Bug fixed profile picture

--- InstaClone/app/Profile.php
+++ InstaClone/app/Profile.php
@@ -8,6 +8,12 @@ class Profile extends Model
 {
 
     protected $guarded = [];
+
+    public function profileImage(){
+        $imagePath = ($this->image) ?  $this->image : '/profile/L69Ty6WwTzythbCUgozJ1bKfCTGDJ4TtsRTCG5me.png';
+        return '/storage/' . $imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
