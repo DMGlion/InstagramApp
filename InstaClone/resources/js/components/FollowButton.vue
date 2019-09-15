4156a87af4aa897eb603b8466ce34c1f9d538300
David Moya <davidmgarcia6@gmail.com>
2019-09-15 20:23:53 +0100

    Follow button added to view component

---
+++ InstaClone/resources/js/components/FollowButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <button class="btn btn-primary ml-4" @click="followUser">Follow</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+
+        methods:{
+            followUser(){
+                alert('Some one is following you');
+            }
+        }
+
+
+    }
+</script>
