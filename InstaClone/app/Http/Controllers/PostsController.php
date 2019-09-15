f0629d39d99507937c892b3d69535f92f1227eba
David Moya <davidmgarcia6@gmail.com>
2019-09-15 12:52:01 +0100

    Create a post

--- InstaClone/app/Http/Controllers/PostsController.php
+++ InstaClone/app/Http/Controllers/PostsController.php
@@ -6,8 +6,29 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    //we add a construct to auth the user to see if it is log in, on this way we will avoid people to log  in without auth
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function create()
     {
         return view('posts.create');
+    }
+
+    public function store()
+    {
+        //Validate the new Post
+        $data = request()->validate([
+            'caption' => 'required',
+            'image' => ['required', 'image'],
+        ]);
+
+        //Create the post
+        //go to auth user get theirs posts and create, on this way it will get the id user
+        auth()->user()->posts()->create($data);
+
+        dd(request()->all());
     }
 }
--- InstaClone/resources/views/posts/create.blade.php
+++ InstaClone/resources/views/posts/create.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 <div class="container">
-    <form action="">
+    <form action="/p" enctype="multipart/form-data" method="post">
+        @csrf
         <div class="row">
             <div class="col-8 offset-2">
 
@@ -13,9 +14,11 @@
                 <div class="form-group row">
                     <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
 
-                    <input id="caption" type="text" class="form-control @error('caption')
-                        is-invalid @enderror" name="caption" value="{{ old('caption') }}"
-                           required autocomplete="caption" autofocus>
+                    <input id="caption"
+                           type="text"
+                           class="form-control @error('caption') is-invalid @enderror" name="caption"
+                           value="{{ old('caption') }}"
+                           autocomplete="caption" autofocus>
                     @error('caption')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
@@ -28,9 +31,7 @@
                     <input type="file", class="form-control-file" id="image" name="image">
 
                     @error('image')
-                    <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
-                    </span>
                     @enderror
                 </div>
                 <div class="row pt-4">
