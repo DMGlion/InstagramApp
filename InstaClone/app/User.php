a0e5aee3320d35dc040d75e73e65425d4babf213
David Moya <davidmgarcia6@gmail.com>
2019-09-16 12:15:22 +0100

    Created new welcome meail class

--- InstaClone/app/User.php
+++ InstaClone/app/User.php
@@ -45,8 +45,9 @@ class User extends Authenticatable
                $user->profile()->create([
                    'title'=>$user->username,
                ]);
-            }
-        );
+
+
+            });
     }
 
     public function posts(){
